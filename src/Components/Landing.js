eec064c2c050b2ccc69aab2a38fa65b48188652e
svsarahval <svsarahval@gmail.com>
2023-03-25 07:24:31 -0400

    just getting started or react clone Landing page

---
+++ src/Components/Landing.js
@@ -0,0 +1,15 @@
+import react from 'react';
+import './components/Landing.css';
+
+function Landing() {
+  return (
+    <div className='firstTitle'>
+      <h1>
+        Welcome to Sociable
+        <br />
+        <br />A place where anything is possible
+      </h1>
+    </div>
+  );
+}
+export default Landing;
